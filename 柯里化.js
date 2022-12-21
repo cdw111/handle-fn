0b868bf3fcf9e6376b579007076683e67c9b93c8
cdw <1370909027@qq.com>
2022-12-21 08:47:46 +0800

    #7

---
+++ 柯里化.js
@@ -0,0 +1,24 @@
+// 用法如下：
+// const add = (a, b, c) => a + b + c;
+// const a = currying(add, 1);
+// console.log(a(2,3))
+function curry(fn, ...args) {
+  const argsSum = fn.length;
+  console.log(argsSum);
+  const curargs = [...args];
+  return function curryTemp(...args) {
+    curargs.push(...args);
+    if (curargs.length >= argsSum) {
+      return fn(...curargs);
+    } else {
+      return curryTemp;
+    }
+  };
+}
+
+const add = (a, b, c) => {
+  console.log(a, b, c);
+  return a + b + c;
+};
+const a = curry(add, 1);
+console.log(a(2, 3));
