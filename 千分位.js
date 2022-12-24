28814a9d567fb47950f99bc8d551d6206202dd7c
cdw <1370909027@qq.com>
2022-12-24 10:19:57 +0800

    #10

---
+++ 千分位.js
@@ -0,0 +1,16 @@
+function formatNumber(num) {
+  var source = String(num).split("."); //按小数点分成2部分
+  source[0] = source[0].replace(new RegExp("(\\d)(?=(\\d{3})+$)", "ig"), "$1,"); //只将整数部分进行逗号分割
+  source[1] = source[1].replace(new RegExp("(\\d{3})", "ig"), "$1,");
+  return source.join("."); //再将小数部分合并进来
+}
+
+function trans(nums) {
+  nums = "" + nums;
+  let [fir, sec] = nums.split(".");
+  fir = fir.replace(/(\d)(?=(\d{3})+$)/g, "$1,");
+  sec = sec.replace(/^(\d{3})+/, "$1,");
+  console.log(fir + "." + sec);
+}
+trans(13246465.5454654654);
+console.log(formatNumber(646132131.6465465));
