d13ce04c54600b66f6b894da964d5a27271fd661
cdw <1370909027@qq.com>
2023-01-30 10:16:53 +0800

    #46

---
+++ new.js
@@ -0,0 +1,10 @@
+function myNew(fn, ...args) {
+  let obj = Object.create(fn.prototype);
+  let res = fn.call(obj, ...args);
+  if (res && (typeof res === "object" || typeof res === "function")) {
+    return res;
+  }
+  return obj;
+}
+
+console.log(Object.prototype.toString.call(1).slice(8, -1));
