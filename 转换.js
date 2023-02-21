7ea2f72efdbb088f7ea1c75463d86cdc0edddcde
cdw <1370909027@qq.com>
2023-02-21 09:08:07 +0800

    #65

--- 转换.js
+++ 转换.js
@@ -13,24 +13,39 @@ const root = [
   },
 ];
 
-function trans(root) {
-  let set = [];
-  root.forEach((element) => {
-    set.push(...element.categories);
-  });
-  set = [...new Set(set)];
-  console.log(set);
-  return set.map((it) => {
-    const temp = {};
-    temp.name = it;
-    temp.categories = [];
-    for (let r of root) {
-      if (r.categories.includes(it)) {
-        temp.categories.push(r.name);
+// function trans(root) {
+//   let set = [];
+//   root.forEach((element) => {
+//     set.push(...element.categories);
+//   });
+//   set = [...new Set(set)];
+//   console.log(set);
+//   return set.map((it) => {
+//     const temp = {};
+//     temp.name = it;
+//     temp.categories = [];
+//     for (let r of root) {
+//       if (r.categories.includes(it)) {
+//         temp.categories.push(r.name);
+//       }
+//     }
+//     return temp;
+//   });
+// }
+
+function trans(arrs) {
+  const result = [];
+  for (let arr of arrs) {
+    arr.categories.forEach((element) => {
+      let res;
+      if ((res = result.find((it) => it.name === element))) {
+        res.categories.push(arr.name);
+      } else {
+        result.push({ name: element, categories: [arr.name] });
       }
-    }
-    return temp;
-  });
+    });
+  }
+  return result;
 }
 
 console.log(trans(root));
