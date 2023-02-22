d29a7568014a2ffe1eec378d1db997631204589a
cdw <1370909027@qq.com>
2023-02-22 11:05:37 +0800

    #66

--- 柯里化.js
+++ 柯里化.js
@@ -2,19 +2,19 @@
 // const add = (a, b, c) => a + b + c;
 // const a = currying(add, 1);
 // console.log(a(2,3))
-function curry(fn, ...args) {
-  const argsSum = fn.length;
-  console.log(argsSum);
-  const curargs = [...args];
-  return function curryTemp(...args) {
-    curargs.push(...args);
-    if (curargs.length >= argsSum) {
-      return fn(...curargs);
-    } else {
-      return curryTemp;
-    }
-  };
-}
+// function curry(fn, ...args) {
+//   const argsSum = fn.length;
+//   console.log(argsSum);
+//   const curargs = [...args];
+//   return function curryTemp(...args) {
+//     curargs.push(...args);
+//     if (curargs.length >= argsSum) {
+//       return fn(...curargs);
+//     } else {
+//       return curryTemp;
+//     }
+//   };
+// }
 
 const add = (a, b, c) => {
   console.log(a, b, c);
@@ -22,3 +22,14 @@ const add = (a, b, c) => {
 };
 const a = curry(add, 1);
 console.log(a(2, 3));
+function curry(fn, ...init) {
+  const res = [...init];
+  const result = (...args) => {
+    res.push(...args);
+    if (res.length >= fn.length) return fn(...res);
+    else {
+      return result;
+    }
+  };
+  return result;
+}
