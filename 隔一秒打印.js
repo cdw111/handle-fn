75f7de19f6e89d7d7563cfdcafd1e6340740fe65
cdw <1370909027@qq.com>
2023-03-11 09:38:18 +0800

    #83

--- 隔一秒打印.js
+++ 隔一秒打印.js
@@ -1,12 +1,23 @@
-async function todu(arr) {
-  for (let i = 0; i < arr.length; i++) {
-    await new Promise((res) =>
-      setTimeout(() => {
-        console.log(arr[i]);
-        res();
-      }, 1000)
-    );
-  }
-}
+// async function todu(arr) {
+//   for (let i = 0; i < arr.length; i++) {
+//     await new Promise((res) =>
+//       setTimeout(() => {
+//         console.log(arr[i]);
+//         res();
+//       }, 1000)
+//     );
+//   }
+// }
 
 todu([1, 2, 3, 4, 5, 6, 7]);
+async function todu(arr) {
+  for (let i = 0; i < arr.length; i++) {
+    const res = await new Promise((res) => {
+      setTimeout(() => {
+        console.log(arr[i]);
+        res(arr[i]);
+      }, 1000);
+    });
+    console.log(res);
+  }
+}
